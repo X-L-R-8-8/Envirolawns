083c4e2747e8e7639af19540fde35be718c5ebc0
X-L-R-8-8 <cc56@mahurangi.school.nz>
2023-05-16 15:03:42 +1200

    Verison 2

---
+++ banner_nav.php
@@ -0,0 +1,19 @@
+<div class="header">
+
+
+	<h1 style="font-size:75px;">
+		Enviro Lawns 
+	</h1>
+</div>
+	<div class="topnav">
+
+		<a href="#home">Home</a>
+		<a href="#about">About me</a>
+		<a href="#services">Services</a>
+		<a href="gallery.php">Gallery</a>
+		<a href="#testimonials">Testimonials</a>
+    <a href="#footer">Contact me</a>
+
+	</div>
+	<!--/ Navagation buttons divisions -->
+
